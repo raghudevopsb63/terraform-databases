75e928dcc5d4e73853341ac56b330c7604f31824
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 06:37:23 +0530

    Update terraform/terraform-databases/

--- main.tf
+++ main.tf
@@ -1,21 +1,36 @@
 module "mongodb" {
-  source         = "./vendor/modules/mongodb"
-  ENV            = var.ENV
-  WORKSTATION_IP = var.WORKSTATION_IP
+  source                    = "./vendor/modules/mongodb"
+  ENV                       = var.ENV
+  WORKSTATION_IP            = var.WORKSTATION_IP
+  DOCUMENTDB_PORT           = var.DOCUMENTDB_PORT
+  DOCUMENTDB_INSTANCE_CLASS = var.DOCUMENTDB_INSTANCE_CLASS
+  DOCUMENTDB_INSTANCE_COUNT = var.DOCUMENTDB_INSTANCE_COUNT
 }
 
 module "redis" {
-  source = "./vendor/modules/redis"
-  ENV    = var.ENV
+  source                     = "./vendor/modules/redis"
+  ENV                        = var.ENV
+  ELASTICACHE_NODE_TYPE      = var.ELASTICACHE_NODE_TYPE
+  ELASTICACHE_NODE_COUNT     = var.ELASTICACHE_NODE_COUNT
+  ELASTICACHE_PORT           = var.ELASTICACHE_PORT
+  ELASTICACHE_ENGINE_VERSION = var.ELASTICACHE_ENGINE_VERSION
 }
 
 module "mysql" {
-  source         = "./vendor/modules/mysql"
-  ENV            = var.ENV
-  WORKSTATION_IP = var.WORKSTATION_IP
+  source             = "./vendor/modules/mysql"
+  ENV                = var.ENV
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  RDS_MYSQL_PORT     = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE  = var.RDS_MYSQL_STORAGE
+  RDS_ENGINE_VERSION = var.RDS_ENGINE_VERSION
+  RDS_INSTANCE_TYPE  = var.RDS_INSTANCE_TYPE
 }
 
 module "rabbitmq" {
-  source = "./vendor/modules/rabbitmq"
-  ENV    = var.ENV
+  source                  = "./vendor/modules/rabbitmq"
+  ENV                     = var.ENV
+  RABBITMQ_PORT           = var.RABBITMQ_PORT
+  RABBITMQ_ENGINE_VERSION = var.RABBITMQ_ENGINE_VERSION
+  RABBITMQ_INSTANCE_TYPE  = var.RABBITMQ_INSTANCE_TYPE
 }
+
--- vars.tf
+++ vars.tf
@@ -1,2 +1,20 @@
 variable "ENV" {}
 variable "WORKSTATION_IP" {}
+
+variable "RDS_MYSQL_PORT" {}
+variable "RDS_MYSQL_STORAGE" {}
+variable "RDS_ENGINE_VERSION" {}
+variable "RDS_INSTANCE_TYPE" {}
+
+variable "DOCUMENTDB_PORT" {}
+variable "DOCUMENTDB_INSTANCE_CLASS" {}
+variable "DOCUMENTDB_INSTANCE_COUNT" {}
+
+variable "RABBITMQ_PORT" {}
+variable "RABBITMQ_ENGINE_VERSION" {}
+variable "RABBITMQ_INSTANCE_TYPE" {}
+
+variable "ELASTICACHE_NODE_TYPE" {}
+variable "ELASTICACHE_NODE_COUNT" {}
+variable "ELASTICACHE_PORT" {}
+variable "ELASTICACHE_ENGINE_VERSION" {}
